da9fedbb65768dd998524bd9c6c841f6deed7b25
AdityaBVerma <adityabverma1@gmail.com>
2024-07-23 08:43:06 +0530

    auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js"
+import { registerUser } from "../controllers/user.controller.js";
 
 
 const router = Router()
@@ -15,3 +16,5 @@ router.route("/register").post(upload.fields([
         maxCount: 1
     }
 ]), registerUser)
+
+export default router
