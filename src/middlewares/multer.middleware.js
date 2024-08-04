2d79611d1fa970f724fdf084157cd7db0fe66f44
AdityaBVerma <adityabverma1@gmail.com>
2024-08-04 14:36:31 +0530

    multer filefilter

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,31 @@
 import multer from "multer";
+import path from "path"
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, "./public/temp")// cb is for callback and null is for error mostly works fine as never returns error
     },
     filename: function (req, file, cb) {
-        cb(null, file.originalname)
+        cb(null, `${Date.now()}-${file.originalname}`)
     }
 })
 
-export const upload = multer({ storage })
+const fileFilter = (req, file, cb) => {
+    const filetypes = /jpeg|jpg|png|gif|mp4|mov|mkv|pdf|doc|docx/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+    const mimetype = filetypes.test(file.mimetype)
+
+    if(extname && mimetype){
+        return cb(null, true);
+    } else {
+        cb(new Error('Unsupported file type'), false);
+    }
+}
+
+export const upload = multer({
+    storage,
+    fileFilter,
+    limits: { 
+        fileSize: 1024 * 1024 * 50 
+    }
+})
