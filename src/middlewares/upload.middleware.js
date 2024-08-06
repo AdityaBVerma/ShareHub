201d58452100c8b8303ebb43b6271137517c993b
AdityaBVerma <adityabverma1@gmail.com>
2024-08-06 08:53:24 +0530

    upload middleware for checking resourcetype

---
+++ src/middlewares/upload.middleware.js
@@ -0,0 +1,31 @@
+import { ApiError } from "../utils/ApiError.js"
+import asyncHandler from "../utils/asyncHandler.js"
+import { upload } from "./multer.middleware.js"
+
+export const uploadType = asyncHandler( (req, res, next) => {
+    const {resourceType} = req.params
+    let fieldname
+    switch (resourceType) {
+        case 'videos':
+            fieldname = 'videofile'
+            break;
+        case 'images':
+            fieldname = 'imagefile'
+            break;
+        case 'docs':
+            fieldname = 'docfile'
+            break;
+        default:
+            throw new ApiError(400, "Wrong resourcetype in upload middleware")
+            
+    }
+    upload.single(fieldname)(req, res, (err) => {
+        if (err) {
+            return next(new ApiError(400, err?.message || "Error in upload multer"));
+        }
+        if (!req.file) {
+            return next(new ApiError(400, "Upload not successful"));
+        }
+        next()
+    })
+})
