fa7282250beaf32b24f91b58384b3e6767b267cc
AdityaBVerma <adityabverma1@gmail.com>
2024-08-24 16:40:28 +0530

    function to generate access and refresh token in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,20 @@ import { ApiError } from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 
+const generateAccessAndRefreshToken = async (userid) =>{
+    try {
+        const user = await User.findById(userid)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+        user.refreshToken = refreshToken
+        await user.save({validateBeforeSave: false})
+    
+        return {accessToken, refreshToken}
+    } catch (error) {
+        throw new ApiError(500, "Unable to generate access and refresh tokens")
+    }
+}
+
 const registerUser = asyncHandler( async (req, res) => {
     const {username, email, fullName, password} = req.body
 
