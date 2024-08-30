6b298eb182157d592e39be391fa35f2f6e22037d
AdityaBVerma <adityabverma1@gmail.com>
2024-08-30 22:54:18 +0530

    createNewGroup group controller

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -1,8 +1,45 @@
+import { isValidObjectId } from "mongoose";
 import asyncHandler from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { Instance } from "../models/instance.model.js"
+import { Group } from "../models/group.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
-const getAllGroups = asyncHandler( async (req, res) => {})
-
-const createNewGroup = asyncHandler( async (req, res) => {})
+const createNewGroup = asyncHandler( async (req, res) => {
+    const {instanceId} = req.params
+    const { password, name } = req.body
+    if (!(instanceId && isValidObjectId(instanceId))) {
+        throw new ApiError(400, "Invalid instance Id")
+    }
+    const instance = await Instance.findById(instanceId)
+    if (instance.owner.toString()!==req.user._id.toString()) {
+        if (instance.isPrivate === "private") {
+            if (!password || password.trim() === "") {
+                throw new ApiError(400, "Password is required");
+            }
+            const isPasswordCorrect = await instance.isPasswordCorrect(password);
+            if (!isPasswordCorrect) {
+                throw new ApiError(400, "Invalid password");
+            }
+        }
+    }
+    if (name.trim()==="") {
+        throw new ApiError(400, "name is required")
+    }
+    const group = await Group.create(
+        {
+            name: name.trim(),
+            ownedInstance: instanceId,
+            owner: req.user._id
+        }
+    )
+    if (!group) {
+        throw new ApiError(400, "Error in creating a group")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, group, "Group created successfully"))
+})
 
 const getGroupById = asyncHandler( async (req, res) => {})
 
@@ -10,8 +47,9 @@ const updateGroup = asyncHandler( async (req, res) => {})
 
 const deleteGroup = asyncHandler( async (req, res) => {})
 
+//move group from one instance to another
+
 export {
-    getAllGroups,
     createNewGroup,
     getGroupById,
     updateGroup,
