68a4faf54c4676d5e2c629d7b7539c18294b2858
AdityaBVerma <adityabverma1@gmail.com>
2024-08-31 16:39:15 +0530

    updateGroup group controller

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -145,7 +145,54 @@ const getGroupById = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, group[0], "Group fetched successfully"))
 })
 
-const updateGroup = asyncHandler( async (req, res) => {})
+const updateGroup = asyncHandler( async (req, res) => {
+    const { instanceId, groupId } = req.params
+    const { name, password } = req.body
+    if (!(instanceId && isValidObjectId(instanceId))) {
+        throw new ApiError(400, "Invalid instanceId")
+    }
+    if (!(groupId && isValidObjectId(groupId))) {
+        throw new ApiError(400, "Invalid Group Id")
+    }
+    const instance = await Instance.findById(instanceId)
+    if (!instance) {
+        throw new ApiError(404 , "Instance not found")
+    }
+    if (instance.owner.toString()!==req.user._id.toString()) {
+        if (instance.isPrivate==="private") {
+            if (!password || password.trim()==="") {
+                throw new ApiError(400, "Password is required")
+            }
+            if (password) {
+                const isPasswordCorrect = await instance.isPasswordCorrect(password)
+                if (!isPasswordCorrect) {
+                    throw new ApiError(400, "Incorrect Password")
+                }
+            }
+        }
+    }
+    if (!name || name.trim()==="") {
+        throw new ApiError(400, "New Name is required")
+    }
+    const updatedGroup = await Group.findByIdAndUpdate(
+        groupId,
+        {
+            $set:{
+                name: name
+            }
+        },
+        {
+            new:true
+        }
+    ).select("-docfiles -imagefiles -videofiles")
+
+    if (!updatedGroup) {
+        throw new ApiError(400, "Could'nt update group")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, updatedGroup, "Group Updated successfully"))
+})
 
 const deleteGroup = asyncHandler( async (req, res) => {})
 
