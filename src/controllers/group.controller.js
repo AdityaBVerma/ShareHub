ad3c1830946c137b2425305a453e3fbcdd90d2a8
AdityaBVerma <adityabverma1@gmail.com>
2024-08-31 15:42:36 +0530

    getGroupById group controller

--- src/controllers/group.controller.js
+++ src/controllers/group.controller.js
@@ -41,7 +41,109 @@ const createNewGroup = asyncHandler( async (req, res) => {
     .json(new ApiResponse(200, group, "Group created successfully"))
 })
 
-const getGroupById = asyncHandler( async (req, res) => {})
+const getGroupById = asyncHandler( async (req, res) => {
+    const { instanceId, groupId } = req.params
+    const { password } = req.body
+    if (!(instanceId && isValidObjectId(instanceId))) {
+        throw new ApiError(400, "Invalid instanceId")
+    }
+    if (!(groupId && isValidObjectId(groupId))) {
+        throw new ApiError(400, "Invalid Group Id")
+    }
+    const instance = await Instance.findById(instanceId)
+    if (!instance) {
+        throw new ApiError(404 , "Instance not found")
+    }
+    if (instance.owner.toString()!==req.user._id.toString()) {
+        if (instance.isPrivate==="private") {
+            if (!password || password.trim()==="") {
+                throw new ApiError(400, "Password is required")
+            }
+            if (password) {
+                const isPasswordCorrect = await instance.isPasswordCorrect(password)
+                if (!isPasswordCorrect) {
+                    throw new ApiError(400, "Incorrect Password")
+                }
+            }
+        }
+    }
+    const group = await Group.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(groupId)
+            }
+        },
+        {
+            $lookup:{
+                from: "docs",
+                localField: "_id",
+                foreignField: "group",
+                as:"docs",
+                pipeline:[
+                    {
+                        $project: {
+                            title:1,
+                            docfile:1
+                        }
+                    },
+                    {
+                        $sort:{
+                            createdAt: -1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup:{
+                from: "videos",
+                localField: "_id",
+                foreignField: "group",
+                as:"videos",
+                pipeline: [
+                    {
+                        $project: {
+                            title: 1,
+                            videofile: 1
+                        }
+                    },
+                    {
+                        $sort: {
+                            createdAt: -1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $lookup:{
+                from:"images",
+                localField: "_id",
+                foreignField:"group",
+                as:"images",
+                pipeline:[
+                    {
+                        $project:{
+                            title:1,
+                            imagefile:1
+                        }
+                    },
+                    {
+                        $sort:{
+                            createdAt: -1
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    if (!group.length) {
+        throw new ApiError(400, "Couldn't get group")
+    }
+    return res
+    .status(200)
+    .json(new ApiResponse(200, group[0], "Group fetched successfully"))
+})
 
 const updateGroup = asyncHandler( async (req, res) => {})
 
