db2ead75023e28312b6af4a4fab96d0cfb6414a6
AdityaBVerma <adityabverma1@gmail.com>
2024-09-03 18:56:36 +0530

    routes for all router defined

--- src/app.js
+++ src/app.js
@@ -14,8 +14,16 @@ app.use(cookieParser())
 app.use(express.static("public"))
 
 import userRouter from "./routes/user.routes.js"
+import instanceRouter from "./routes/instance.routes.js"
+import groupRouter from "./routes/group.routes.js"
+import resourceRouter from "./routes/resource.routes.js"
+import commentRouter from "./routes/comment.routes.js"
 
 
 app.use("/api/v1/users", userRouter)
+app.use("/api/v1/instances", instanceRouter)
+app.use("/api/v1/groups", groupRouter)
+app.use("/api/v1/resources", resourceRouter)
+app.use("/api/v1/comments", commentRouter)
 
 export {app}
